7a179c4cc348382e155eeb6dfec3ad63430b2170
Jodel Asur <jodelasur@gmail.com>
2019-09-19 14:48:51 +0800

    Initial commit

---
+++ login_view_test/urls.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+from django.contrib.auth import views as auth_views
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render
+from django.urls import path, reverse
+
+
+@login_required(login_url='login/')
+def home_view(request):
+    return render(request, 'home.html')
+
+
+urlpatterns = [
+    path('', home_view),
+    path('admin/', admin.site.urls),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+]
